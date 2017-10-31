e950b97d6cc46e619d8eeaff639bb1d7a563e5b5
weaving <tanjibo@zhuozhenxuan.com>
2017-10-31 11:26:43 +0800

    添加模板条状路径

--- config/lrss.php
+++ config/lrss.php
@@ -10,43 +10,34 @@
  */
 
 
-    return [
-        'experience' => [18610729170,13076966542],
-        'wechatSms'  => [
-
-            'app_id'    => 'wx744602f2d362bace',
-            'secret_id' => 'ca2563feb3d4e46eb94dd8bfda3fabe4',
-            'token'     => 'weaving',
-            'open_id'   => 'ohJajxKAue5qiLgaEkgB_4nUx7Xg',
-            # 微信模板发送通知 预约  成功 取消
-
-            'template_id_booking'     => "ETZ04TnlIqMxfyhwpWPGJdyu75m2Gcgd_VG53lunuEY",
-            'template_id_pay_success' => "3Q79kMhTzuOlfftk82cHjhhF8KkeImRznRwdiwt6vPo",
-            'template_id_cancel'     => "ngpwbUoQAt-lVQPuyEISGoGB0bwpMZDjRRrV3L9W_5M",
-        ],
-        'notification'=>[
-            'email'=>"1533954229@qq.com",
-            'from'=>"tanjibo@zhuozhenxuan.com",
-            'address'=>'北京濯振轩文化发展有限公司'
-        ]
-    ];
+//    return [
+//        'experience' => [18610729170,13076966542],
+//        'wechatSms'  => [
+//
+//            'app_id'    => 'wx744602f2d362bace',
+//            'secret_id' => 'ca2563feb3d4e46eb94dd8bfda3fabe4',
+//            'token'     => 'weaving',
+//            'open_id'   => 'ohJajxKAue5qiLgaEkgB_4nUx7Xg',
+//            # 微信模板发送通知 预约  成功 取消
+//
+//            'template_id_booking'     => "ETZ04TnlIqMxfyhwpWPGJdyu75m2Gcgd_VG53lunuEY",
+//            'template_id_pay_success' => "3Q79kMhTzuOlfftk82cHjhhF8KkeImRznRwdiwt6vPo",
+//            'template_id_cancel'     => "ngpwbUoQAt-lVQPuyEISGoGB0bwpMZDjRRrV3L9W_5M",
+//        ],
+//        'notification'=>[
+//            'email'=>"1533954229@qq.com",
+//            'from'=>"tanjibo@zhuozhenxuan.com",
+//            'address'=>'北京濯振轩文化发展有限公司'
+//        ]
+//    ];
 
 return [
 
-    'experience' => [
-        13501221507,
-        13522791887,
-        15067256154,
-        15087281826,
-        13681335569,
-        13601026926,
-        13466662162,
-        13466530215,
-    ],
+    'experience' => explode(',',env('NOTIFY_MOBILE')),
     'wechatSms'  => [
-        'app_id'                  => 'wx8843f404893bd3e6',
-        'secret_id'               => '3114b2e141545a6391c709c86f0e78b2',
-        'token'                   => 'liaorusanshe',
+        'app_id'                  => env('WECHAT_NOTIFY_APPID'),
+        'secret_id'               => env('WECHAT_NOTIFY_SECRET'),
+        'token'                   => env('WECHAT_NOTIFY_TOKEN'),
 
         # 微信模板发送通知 预约  成功 取消
         'template_id_booking'     => "hJ5iCGcWjV_DTpUyFkkUyDhh1VKcHgKSyph6YIjWMKM",
@@ -54,8 +45,8 @@ return [
         'template_id_cancel'      => "xVLfxLr7anfaeYfw6b9_maDc_JSUZgIm2yiZOjB8Jyw",
     ],
     'notification'=>[
-        'email'=>"xiaolijuan@zhuozhenxuan.com",
-        'from'=>"tanjibo@zhuozhenxuan.com",
+        'email'=>env('NOTIFY_TO_EMAIL'),
+        'from'=>env('NOTIFY_FROM_EMAIL'),
         'address'=>'北京濯振轩文化发展有限公司'
     ]
 ];
