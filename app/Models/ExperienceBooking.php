df027c90d15337bdbd955a727fecd7d373194fe0
weaving <tanjibo@zhuozhenxuan.com>
2018-01-02 17:52:34 +0800

    添加安吉预付金

--- app/Models/ExperienceBooking.php
+++ app/Models/ExperienceBooking.php
@@ -210,6 +210,8 @@ class ExperienceBooking extends Eloquent
             foreach ( $ds as $date ) {
                 // 节日价
                 //预付金
+                echo $r->prepay_percent.'<pre>';
+                echo ($r->prepay_percent/100).'<pre>';
                 $prepay= $isPrepay?($r->prepay_percent/100):1;
                 echo $prepay;
                 $special = ExperienceSpecialPrice::query()->where('experience_room_id', $r->id)->where('date', $date)->value('price');
