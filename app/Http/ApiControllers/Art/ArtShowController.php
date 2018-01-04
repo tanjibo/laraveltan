0d38a3dd59b952ba17226d08b14d53603c13400a
weaving <tanjibo@zhuozhenxuan.com>
2018-01-04 18:16:33 +0800

    重新整个artshow评论

--- app/Http/ApiControllers/Art/ArtShowController.php
+++ app/Http/ApiControllers/Art/ArtShowController.php
@@ -52,7 +52,7 @@ class ArtShowController extends ApiController
        //评论信息
         $comments=$art->comments()->where('parent_id',0)->with(['owner','likes'=>function($query){
             $query->where('user_id',auth()->id());
-        }])->limit(10);
+        }])->limit(10)->get();
 
          $data=['art'=>new ArtShowResource($art),'comment'=>CommentResource::collection($comments)];
 
