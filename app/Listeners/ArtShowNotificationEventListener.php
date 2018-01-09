8724c65e266f71320c3992a143beeb75c45d6345
weaving <tanjibo@zhuozhenxuan.com>
2018-01-09 17:21:14 +0800

    添加微信系统通知

--- app/Listeners/ArtShowNotificationEventListener.php
+++ app/Listeners/ArtShowNotificationEventListener.php
@@ -33,7 +33,8 @@ class ArtShowNotificationEventListener
     {
 
         $data=[
-            'art_open_id'=>$event->comment->replies_to_user->owner->art_open_id,
+//            'art_open_id'=>$event->comment->replies_to_user->owner->art_open_id,
+            'art_open_id'=>'oKsQH0Z9BfF5fXGc5KbbdGnnf-Yw',
             'form_id'=>$this->request->form_id,
             'reply_user'=>$event->user->nickname,
             'parent_comment_id'=>$event->comment->parent_id,
