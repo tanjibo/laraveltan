15c94af0ac4b26c697f4ac97d55f6fe400ec14b0
weaving <tanjibo@zhuozhenxuan.com>
2018-01-09 09:07:51 +0800

    重新整个artshow评论

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,11 @@
 let mix = require('laravel-mix');
 
+mix.options({
+
+    devtool: 'cheap'
+
+});
+
 /*pp
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,7 +18,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
- .sass('resources/assets/sass/app.scss', 'public/css').extract(['vue','element-ui']).version();
+  .sass('resources/assets/sass/app.scss', 'public/css').extract(['vue','element-ui']).version();
 // .sass('resources/assets/sass/app.scss', 'public/css');
 
 mix.styles([
