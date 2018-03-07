e7fb99ce4aad42e57daa10e12fc92bf1b461a122
weaving <tanjibo@zhuozhenxuan.com>
2018-03-07 14:14:18 +0800

    添加三舍，发现两个小功能

--- app/Http/Resources/Experience/ArticleResource.php
+++ app/Http/Resources/Experience/ArticleResource.php
@@ -17,7 +17,7 @@ class ArticleResource extends Resource
     {
         return [
             'id'=>$this->id,
-            'cover_img' => $this->type==ExperienceArticle::TYPE_CUSTOMER_STORY_CHILD?$this->cover_img."?imageMogr2/gravity/Center/crop/61x61":$this->cover_img."?imageView2/2/w/750",
+            'cover_img' => $this->type==ExperienceArticle::TYPE_CUSTOMER_STORY_CHILD?$this->cover_img."?imageMogr2/gravity/Center/crop/61x61":$this->cover_img."?imageView2/2/w/375",
             'title'     => $this->title,
             'desc'      => $this->desc,
             'content'   =>  $this->when($request->detail,$this->content),
