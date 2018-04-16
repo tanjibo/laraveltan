ce76d0a449cc7dbc83e4b59be00a095cd78c1491
weaving <tanjibo@zhuozhenxuan.com>
2018-04-16 18:58:57 +0800

    修改点击事件

--- app/Models/Partners.php
+++ app/Models/Partners.php
@@ -27,7 +27,7 @@ class Partners extends Model
         return $query->where('token', $token);
     }
 
-    public static function partnerId( string $token )
+    public static function partnerId( $token )
     {
         return static::query()->token($token)->value('id');
     }
