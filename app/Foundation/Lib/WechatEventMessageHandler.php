e5a4740fa574a7d5efeace405671f5dd49ce3b9a
weaving <tanjibo@zhuozhenxuan.com>
2018-06-01 16:33:02 +0800

    添加自定义中间件

--- app/Foundation/Lib/WechatEventMessageHandler.php
+++ app/Foundation/Lib/WechatEventMessageHandler.php
@@ -147,7 +147,7 @@ class WechatEventMessageHandler implements EventHandlerInterface
     private static function parseParams( $message )
     {
         //{"scene":{"scene_str":"ohJajxKAue5qiLgaEkgB_4nUx7Xg","official_activity_id":1}}}
-        Log::info($message);
+        Log::error($message);
         return json_decode($message[ 'EventKey' ], true)[ 'action_info' ][ 'scene' ];
     }
 
